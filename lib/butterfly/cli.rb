a503d765710d4e7c68550c13783cb456e2d0d953
Gigi M <mmgamao1@gmail.com>
2019-09-26 17:42:44 +0000

    build cli content

--- lib/butterfly/cli.rb
+++ lib/butterfly/cli.rb
@@ -1,7 +1,12 @@
 class Butterfly::CLI 
   
   def start  # instance method
-    puts "Hello World!"
+    puts "**************  Welcome to my furniture shop!  ****************"
+    puts "***** Let us help you find the right living room seating for your home. *****"
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    puts "* Please select from the following options and enter the corresponding number:"
+    puts "* 1) Product name, 2) Product type, 3) Price range, 4) Reviews"
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   end
   
 end
