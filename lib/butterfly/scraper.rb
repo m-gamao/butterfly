063ab77ea168bbb8903e26bb173439226d6e7e96
Gigi M <mmgamao1@gmail.com>
2019-09-28 14:31:24 +0000

    new changes to scraper

--- lib/butterfly/scraper.rb
+++ lib/butterfly/scraper.rb
@@ -1,7 +1,13 @@
 class Butterfly::Scraper 
   def self.scrape_chairs
     doc = Nokogiri::HTML(open("https://www.roomandboard.com/catalog/living/sofas-and-loveseats"))
-    binding.pry
+    
+    chairs = doc.css("div.pg-name")
+    
+    chairs.each do |c|
+      name = c.text  
+      Butterfly::Chair.new(name)
+    end
   end
 end 
 
