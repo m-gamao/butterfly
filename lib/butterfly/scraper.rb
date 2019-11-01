aefdee6b18a0225e1bd3a6cbbbb02676030a1f96
Gigi M <mmgamao1@gmail.com>
2019-11-01 17:20:21 +0000

    final

--- lib/butterfly/scraper.rb
+++ lib/butterfly/scraper.rb
@@ -29,30 +29,3 @@ class Butterfly::Scraper
   #   end
   # end 
 
-#chair.rb
-#contains all the chair names
-class Butterfly::Chair
-  @@all = []
-  attr_accessor :name, :details
-  
-  def initialize(name, details)
-    @name = name 
-    @details = details
-    save
-  end
-  
-  def self.all
-    Butterfly::Scraper.scrape_chairs if @@all.empty?
-    @@all
-  end
-  
-  def save
-    @@all << self
-  end
-
-    
-    
-    
-    
-end
-end
